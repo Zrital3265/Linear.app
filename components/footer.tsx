af22a09018fc4cc2880003f9352dbc6c693b0bf6
Zrital <Ztrital12580@gmail.com>
2024-05-09 08:54:29 +0545

    footer added with responsiveness

--- components/footer.tsx
+++ components/footer.tsx
@@ -82,7 +82,7 @@ export const Footer = () => {
                     ) => (
                       <li key={linkIndex} className="[&_a]:last:mb-0">
                         <Link
-                          className="mb-3 block text-grey hover:text-white"
+                          className="mb-3 block text-grey transition-colors hover:text-white"
                           href={link.href}
                         >
                           {link.title}
