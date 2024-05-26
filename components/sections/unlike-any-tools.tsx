20e66b13f2c108fda9c2d7a1bd35e9c1b2e05192
Zrital <Ztrital12580@gmail.com>
2024-05-26 07:40:30 +0545

    added usp section

--- components/sections/unlike-any-tools.tsx
+++ components/sections/unlike-any-tools.tsx
@@ -1,5 +1,16 @@
 export const UnlikeAnyTools = () => (
   <>
-    <h2>Unlike any tools that you haev used before.</h2>
+    <div className="text-white">
+      <div className="text-center">
+        <h2 className="mb-4 text-4xl md:mb-7 md:text-7xl">
+          Unlike any tools <br className="hidden md:inline-block" />
+          that you have used before.
+        </h2>
+        <p className="mx-auto mb-4 max-w-[68rem] text-lg text-primary-text md:text-xl">
+          Designed to the last pixel and engineered with unforgiving precision,
+          Linear combines UI elegance with world-class performance.
+        </p>
+      </div>
+    </div>
   </>
 );
