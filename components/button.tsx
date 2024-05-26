fdcf325d0a80e2fa4a58d906b9be5fda205d95f1
Zrital <Ztrital12580@gmail.com>
2024-05-26 16:08:24 +0545

    DMST section added with hover button effect

--- components/button.tsx
+++ components/button.tsx
@@ -4,7 +4,7 @@ import React from "react";
 interface buttonProps extends VariantProps<typeof buttonClasses> {
   children: React.ReactNode;
   href: string;
-  className: string;
+  className?: string;
 }
 const buttonClasses = cva("rounded-full inline-flex items-center", {
   variants: {
@@ -15,7 +15,7 @@ const buttonClasses = cva("rounded-full inline-flex items-center", {
       ],
       secondary: [
         "text-off-white bg-white bg-opacity-10 border border-transparent-white backdrop-filter-[12px] hover:bg-opacity-20 transition-colors ease-in",
-        " [&_.icon-wrapper]:bg-transparent-white [&_.icon-wrapper]:rounded-full [&_.icon-wrapper]:ml-2 [&_.icon-wrapper]:px-2 [&_.icon-wrapper]:-mr-2    ",
+        " [&_.icon-wrapper]:bg-transparent-white [&_.icon-wrapper]:rounded-full [&_.icon-wrapper:last-child]:ml-2 [&_.icon-wrapper]:px-2 [&_.icon-wrapper:last-child]:-mr-2  [&_.icon-wrapper:first-child]:mr-2 [&_.icon-wrapper:first-child]:-ml-2 ",
       ],
       teritary: "",
     },
--- components/sections/unlike-any-tools.tsx
+++ components/sections/unlike-any-tools.tsx
@@ -1,3 +1,5 @@
+import { Button, IconWrapper } from "../button";
+import { LogoLightIllustration } from "../illustration/logolight";
 import { ZapIllustration } from "../illustration/zap";
 
 export const UnlikeAnyTools = () => (
@@ -14,7 +16,7 @@ export const UnlikeAnyTools = () => (
         </p>
       </div>
       <div className="flex flex-wrap gap-6">
-        <div className="bg-glass-gradient flex min-h-[48rem] basis-[calc(66.66%-12px)]  flex-col justify-end rounded-[4.8rem] border border-transparent-white p-8 text-center md:p-14">
+        <div className="bg-glass-gradient flex min-h-[48rem] basis-[calc(66.66%-12px)] flex-col  items-center justify-end rounded-[4.8rem] border border-transparent-white p-8 text-center md:p-14">
           <p className="text-3xl">Built for your keyboard</p>
           <p className="text-md text-primary-text">
             Fly through your tasks with rapid-fire keyboard shortcuts for
@@ -30,13 +32,33 @@ export const UnlikeAnyTools = () => (
             Built for speed with 50ms interactions and real-time sync.
           </p>
         </div>
-        <div className="bg-glass-gradient flex min-h-[48rem] basis-[calc(33.33%-12px)]  flex-col justify-end rounded-[4.8rem] border border-transparent-white p-8 text-center md:p-14">
+        <div className=" bg-glass-gradient group relative flex min-h-[48rem] basis-[calc(33.33%-12px)] flex-col  items-center justify-end rounded-[4.8rem] border border-transparent-white p-8 text-center md:p-14">
+          <div className="pointer-events-none absolute top-[-8rem] w-[130%]">
+            <LogoLightIllustration />
+          </div>
           <p className="text-3xl">Designed for modern software teams</p>
           <p className="text-md text-primary-text">
             Comes with built-in workflows that create focus and routine.
           </p>
+          <Button
+            variant="secondary"
+            href="/"
+            className="absolute bottom-[20rem] translate-y-[30%] scale-[0.8] opacity-0 transition-[transform,opcity] group-hover:transform-none group-hover:opacity-100 [&.IconWrapper]:text-[-1.1rem]"
+          >
+            <IconWrapper>Linear Method</IconWrapper>
+            Product principles
+            <svg
+              className="ml-1"
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="#8A8F98"
+            >
+              <path d="M5.46967 11.4697C5.17678 11.7626 5.17678 12.2374 5.46967 12.5303C5.76256 12.8232 6.23744 12.8232 6.53033 12.5303L10.5303 8.53033C10.8207 8.23999 10.8236 7.77014 10.5368 7.47624L6.63419 3.47624C6.34492 3.17976 5.87009 3.17391 5.57361 3.46318C5.27713 3.75244 5.27128 4.22728 5.56054 4.52376L8.94583 7.99351L5.46967 11.4697Z"></path>
+            </svg>
+          </Button>
         </div>
-        <div className="bg-glass-gradient flex min-h-[48rem] basis-[calc(66.66%-12px)]  flex-col justify-end rounded-[4.8rem] border border-transparent-white p-8 text-center md:p-14">
+        <div className="bg-glass-gradient flex min-h-[48rem] basis-[calc(66.66%-12px)] flex-col items-center justify-end rounded-[4.8rem] border border-transparent-white p-8 text-center md:p-14">
           <p className="text-3xl">Meet your command line</p>
           <p className="text-md text-primary-text">
             Complete any action in seconds with the global command menu.
