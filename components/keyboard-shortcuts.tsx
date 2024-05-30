b618961e5482c303e84a762d4883747f98901bb4
Zrital <Ztrital12580@gmail.com>
2024-05-30 10:20:34 +0545

    keyboard svg add

---
+++ components/keyboard-shortcuts.tsx
@@ -0,0 +1,9 @@
+import { KeyboardIllustration } from "./illustration/keyboard";
+
+export const KeyboardShortcuts = () => {
+  return (
+    <div className="mask-keyboard h-full w-full">
+      <KeyboardIllustration />;
+    </div>
+  );
+};
